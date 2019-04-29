fc1e1b30070a40279f4ef5ac1fcc4df52bc82a8b
Michael Dockter <michael@dockter.com>
2019-04-29 17:22:39 -0400

    issue-1 Tweek

--- rootfs/app/sleep-infinity.sh
+++ rootfs/app/sleep-infinity.sh
@@ -10,8 +10,13 @@ handle_term()
 
 trap 'handle_term' TERM
 
+# =============================================================================
 # Main
+# =============================================================================
 
 echo "/app/sleep-infinitely.sh is sleeping infinitely."
+
+# Sleep in a manner that allows "docker stop ..." to shutdown gracefully.
+
 sleep infinity &
 wait
